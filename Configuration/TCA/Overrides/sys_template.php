1a2a389d00b57da06602fbbf652024ce86de1e23
Lars Tiefland <l.tiefland@weban.de>
2023-09-25 14:38:47 +0200

    angepasst

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,4 +4,4 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3_MODE') or die();
 
-ExtensionManagementUtility::addStaticFile('tft', 'Configuration/TypoScript/', 'NWTU');
+ExtensionManagementUtility::addStaticFile('tft', 'Configuration/TypoScript/', 'TFT');
